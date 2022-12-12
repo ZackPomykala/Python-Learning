3a2915f43814b68485155f5db0126b71818e9772
ZackPomykala <83101453+ZackPomykala@users.noreply.github.com>
2022-12-12 14:08:31 -0700

    Add files via upload

---
+++ FizzBuzz.py
@@ -0,0 +1,11 @@
+# Doesn't use an array, may be a simplistic solution
+
+for number in range(1, 101):
+    if number % 3 == 0 and number % 5 == 0:
+        print("FizzBuzz")
+    elif number % 3 == 0:
+        print("Fizz")
+    elif number % 5 == 0:
+        print("Buzz")
+    else:
+        print(number)
